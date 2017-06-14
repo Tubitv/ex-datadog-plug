8bc9be7d1bd70d1ec7cd7d6f93df8ef7aea6473e
Tyr Chen <tyr@adrise.com>
2017-06-14 17:55:12 +0800

    distinguish route/path.

--- lib/ex_datadog_plug.ex
+++ lib/ex_datadog_plug.ex
@@ -10,6 +10,8 @@ defmodule ExDatadog.Plug do
       Default is `plug`.
     * `:method` - a boolean value to include the method in the tag list.
       Default is `false`.
+    * `:path` - a boolean value to include the path in the tag list.
+      Default is `false`.
     * `:query` - a list of strings to include specific query string in the tag list. `[]` will generate all query
       params as tags.
       Default is `nil` (do not generate tag for query string)
@@ -37,20 +39,26 @@ defmodule ExDatadog.Plug do
 
   defp gen_tags(conn, opts) do
     include_method? = Keyword.get(opts, :method, false)
+    include_path? = Keyword.get(opts, :path, false)
     query_list = Keyword.get(opts, :query, nil)
 
-    conn.request_path
-      |> gen_path_tags
+    conn.path_info
+      |> gen_route_tags(conn.path_params)
+      |> Enum.concat(gen_path_tags(conn.path_info, include_path?))
       |> Enum.concat(gen_method_tags(conn.method, include_method?))
       |> Enum.concat(gen_query_tags(conn.query_string, query_list))
   end
 
-  defp gen_path_tags("/"), do: ["/"]
-
-  defp gen_path_tags(path) do
-    path = Regex.replace(~r/\/$/, path, "")
-    [path, Path.dirname(path)]
+  defp gen_route_tags(path_info, %{}), do: [join_path(path_info, "route")]
+  defp gen_route_tags(path_info, path_params) do
+    reversed_params = Enum.reduce(path_params, %{}, fn {k, v}, acc -> Map.put(acc, v, k) end)
+    path_info
+      |> Enum.map(fn v -> Map.get(reversed_params, v) || v end)
+      |> gen_route_tags(%{})
   end
+
+  defp gen_path_tags(_path_info, false), do: []
+  defp gen_path_tags(path_info, true), do: [join_path(path_info, "path")]
 
   defp gen_method_tags(_method, false), do: []
   defp gen_method_tags(method, true), do: [method]
@@ -68,4 +76,6 @@ defmodule ExDatadog.Plug do
     end
 
   end
+
+  defp join_path(path_info, prefix), do: "#{prefix}:/#{Enum.join(path_info, "/")}"
 end
